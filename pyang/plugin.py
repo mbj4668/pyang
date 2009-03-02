0f01411356ed93d1d2babb94412342c40a3d07b7
mbj4668 <mbj4668@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2009-03-02 22:07:33 +0000

    added experimental tree output plugin

--- pyang/plugin.py
+++ pyang/plugin.py
@@ -10,12 +10,13 @@ def init(plugindirs=[]):
     """Initialize the plugin framework"""
 
     # initialize the builtin plugins
-    from translators import yang,yin,rng,xsd  #,dsdl
+    from translators import yang,yin,rng,xsd,tree  #,dsdl
     yang.pyang_plugin_init()
     yin.pyang_plugin_init()
     # dsdl.pyang_plugin_init()
     rng.pyang_plugin_init()
     xsd.pyang_plugin_init()
+    tree.pyang_plugin_init()
 
     # search for plugins in std directory
     basedir = os.path.split(sys.modules['pyang'].__file__)[0]
---
+++ pyang/translators/tree.py
@@ -0,0 +1,94 @@
+"""Tree output plugin
+
+Status: experimental (not ready - needs prettier formatting)
+
+Idea copied from libsmi.
+"""
+
+import optparse
+
+from pyang import plugin
+
+def pyang_plugin_init():
+    plugin.register_plugin(TreePlugin())
+
+class TreePlugin(plugin.PyangPlugin):
+    def add_opts(self, optparser):
+        optlist = [
+            optparse.make_option("--tree-no-leafs",
+                                 dest="tree_no_leafs",
+                                 action="store_true",
+                                 help="Do not print any leafs"),
+            ]
+        g = optparser.add_option_group("Tree output specific options")
+        g.add_options(optlist)
+    def add_output_format(self, fmts):
+        fmts['tree'] = self
+    def emit(self, ctx, module, fd):
+        emit_tree(module, fd)
+        
+def emit_tree(module, fd):
+    fd.write("%s: %s\n" % (module.keyword, module.arg))
+    print_children(module, fd, ' ')
+
+def print_children(s, fd, prefix):
+    if hasattr(s, 'i_children'):
+        typewidth = 0
+        for ch in s.i_children:
+            typename = get_typename(ch)
+            if len(typename) > typewidth:
+                typewidth = len(typename)
+        for ch in s.i_children:
+            if ch == s.i_children[-1]:
+                newprefix = prefix + '   '
+            else:
+                newprefix = prefix + '  |'
+            print_node(ch, fd, newprefix, typewidth)
+
+def print_node(s, fd, prefix, typewidth):
+    fd.write("%s%s--" % (prefix[0:-1], get_status_str(s)))
+
+    if s.keyword == 'list':
+        fd.write(s.arg)
+    elif s.keyword == 'container':
+        p = s.search_one('presence')
+        if p is None or p.arg == 'false':
+            fd.write(s.arg)
+        else:
+            fd.write(s.arg + '?')
+    elif s.keyword in ['choice', 'case']:
+        fd.write('(' + s.arg + ')')
+    else:
+        fd.write(" %s %-*s   %s" % (get_flags_str(s), typewidth,
+                                    get_typename(s), s.arg))
+
+    if s.keyword == 'list':
+        fd.write(" [%s]" % s.search_one('key').arg)
+    fd.write('\n')
+    print_children(s, fd, prefix)
+
+def get_status_str(s):
+    status = s.search_one('status')
+    if status is None or status.arg == 'current':
+        return '+'
+    elif status.arg == 'deprecated':
+        return 'x'
+    elif status.arg == 'obsolete':
+        return 'o'
+
+def get_flags_str(s):
+    if s.keyword == 'rpc':
+        return '-x'
+    elif s.keyword == 'notification':
+        return '-n'    
+    elif s.i_config == True:
+        return 'rw'
+    else:
+        return 'ro'
+
+def get_typename(s):
+    t = s.search_one('type')
+    if t is not None:
+        return t.arg
+    else:
+        return ''
