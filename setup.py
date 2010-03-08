d76358c542da4c2554ac3f7545fdcb8fed44ef21
lhotka@cesnet.cz <lhotka@cesnet.cz@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2010-03-08 17:37:41 +0000

    Restructured files for the 2nd step of DSDL.
    yang2dsdl can now be called from any directory.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 import pyang
 import glob
+import os
 
-modules = glob.glob('modules/*.yang')
+modules = glob.glob(os.path.join('modules', '*.yang'))
+xslt = glob.glob(os.path.join('xslt', '*.xsl'))
 
 setup(name='pyang',
       version=pyang.__version__,
@@ -10,8 +12,11 @@ setup(name='pyang',
       author_email='mbj@tail-f.com',
       description="A YANG validator and converter",
       url='http://code.google.com/p/pyang',
-      scripts=['bin/pyang', 'bin/yang2html'],
+      scripts=['bin/pyang', 'bin/yang2html', 'bin/yang2dsdl'],
       packages=['pyang', 'pyang.plugins', 'pyang.translators'],
       data_files=[('share/man/man1', ['man/man1/pyang.1']),
-                  ('share/yang/modules', modules)],
+                  ('share/yang/modules', modules),
+                  ('share/yang/xslt', xslt),
+                  ('share/yang/schema', ['schema/yin.rng',
+                                         'schema/relaxng-lib.rng'])]
       )
