ea8f8e97e24391cd8396cf1e06542063241974c8
mbj4668@gmail.com <mbj4668@gmail.com@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2010-03-16 10:24:12 +0000

    Set yang2dsdl env variables in env.sh; prefixed the vars with PYANG_

--- env.sh
+++ env.sh
@@ -8,4 +8,6 @@ export PATH=$p/bin:$PATH
 export MANPATH=$p/man:$MANPATH
 export PYTHONPATH=$p:$PYTHONPATH
 export YANG_MODPATH=$p/modules:$YANG_MODPATH
+export PYANG_XSLT_DIR=$p/xslt
+export PYANG_RNG_LIBDIR=$p/schema
 export W=$p
