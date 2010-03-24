e6f26894a55320d8f1f6804234756eea7430d2ec
mbj4668 <mbj4668@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2010-03-24 13:19:55 +0000

    (properly) ignore whitespace when tokenizing xpath

--- trunk/pyang/xpath.py
+++ trunk/pyang/xpath.py
@@ -71,6 +71,11 @@ def tokens(s):
             m = r.match(s, pos)
             if m is not None:
                 # found a matching token
+                if tokname == 'whitespace': 
+                    # completely ignore whitespace 
+                    pos += len(m.group(0)) 
+                    matched = True 
+                    continue 
                 prec = _preceding_token(toks)
                 if tokname == '*' and prec is not None and _is_special(prec):
                     # XPath 1.0 spec, 3.7 special rule 1a
