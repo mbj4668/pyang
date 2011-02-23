1f031baba6635873372f224a0291ea113bd6e44e
mbj4668@gmail.com <mbj4668@gmail.com@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2011-02-23 13:38:50 +0000

    Pretty print in yang output plugin

--- pyang/translators/yang.py
+++ pyang/translators/yang.py
@@ -30,9 +30,50 @@ class YANGPlugin(plugin.PyangPlugin):
         emit_yang(ctx, module, fd)
         
 def emit_yang(ctx, module, fd):
-    emit_stmt(ctx, module, fd, '', '  ')
+    emit_stmt(ctx, module, fd, 0, None, '', '  ')
     
-def emit_stmt(ctx, stmt, fd, indent, indentstep):
+
+_force_newline_arg = ('description', 'contact')
+_non_quote_arg_type = ('identifier', 'identifier-ref', 'boolean', 'integer')
+
+_kwd_class = {
+    'yang-version': 'header',
+    'namespace': 'header',
+    'prefix': 'header',
+    'belongs-to': 'header',
+    'organization': 'meta',
+    'contact': 'meta',
+    'description': 'meta',
+    'reference': 'meta',
+    'import': 'linkage',
+    'include': 'linkage',
+    'revision': 'revision',
+    'typedef': 'defs',
+    'grouping': 'defs',
+    'identity': 'defs',
+    'feature': 'defs',
+    'extension': 'defs',
+    'module': None,
+    'submodule': None,
+}
+def get_kwd_class(keyword):
+    if util.is_prefixed(keyword):
+        return 'extension'
+    else:
+        try:
+            return _kwd_class[keyword]
+        except KeyError:
+            return 'body'
+
+_keyword_with_trailing_newline = (
+    'typedef',
+    'grouping',
+    'identity',
+    'feature',
+    'extension',
+    )
+
+def emit_stmt(ctx, stmt, fd, level, prev_kwd_class, indent, indentstep):
     if ctx.opts.yang_remove_unused_imports and stmt.keyword == 'import':
         for p in stmt.parent.i_unused_prefixes:
             if stmt.parent.i_unused_prefixes[p] == stmt:
@@ -43,16 +84,21 @@ def emit_stmt(ctx, stmt, fd, indent, indentstep):
         keyword = prefix + ':' + identifier
     else:
         keyword = stmt.keyword
+    kwd_class = get_kwd_class(stmt.keyword)
+    if ((level == 1 and 
+         kwd_class != prev_kwd_class and kwd_class != 'extension') or
+        stmt.keyword in _keyword_with_trailing_newline):
+        fd.write('\n')
     fd.write(indent + keyword)
     if stmt.arg != None:
         if keyword in grammar.stmt_map:
             (arg_type, _subspec) = grammar.stmt_map[keyword]
-            if arg_type in ['identifier', 'identifier-ref', 'boolean']:
+            if arg_type in _non_quote_arg_type:
                 fd.write(' ' + stmt.arg)
             else:
-                emit_arg(stmt.arg, fd, indent, indentstep)
+                emit_arg(stmt, fd, indent, indentstep)
         else:
-            emit_arg(stmt.arg, fd, indent, indentstep)
+            emit_arg(stmt, fd, indent, indentstep)
     if len(stmt.substmts) == 0:
         fd.write(';\n')
     else:
@@ -61,19 +107,27 @@ def emit_stmt(ctx, stmt, fd, indent, indentstep):
             substmts = grammar.sort_canonical(stmt.keyword, stmt.substmts)
         else:
             substmts = stmt.substmts
+        if level == 0:
+            kwd_class = 'header'
         for s in substmts:
-            emit_stmt(ctx, s, fd, indent + indentstep, indentstep)
+            emit_stmt(ctx, s, fd, level + 1, kwd_class,
+                      indent + indentstep, indentstep)
+            kwd_class = get_kwd_class(s.keyword)
         fd.write(indent + '}\n')
 
-def emit_arg(arg, fd, indent, indentstep):
+def emit_arg(stmt, fd, indent, indentstep):
     """Heuristically pretty print the argument string"""
     # current alg. always print a double quoted string
+    arg = stmt.arg
     arg = arg.replace('\\', r'\\')
     arg = arg.replace('"', r'\"')
     arg = arg.replace('\t', r'\t')
     lines = arg.splitlines()
     if len(lines) <= 1:
-        fd.write(' "' + arg + '"')
+        if stmt.keyword in _force_newline_arg:
+            fd.write('\n' + indent + indentstep + '"' + arg + '"')
+        else:
+            fd.write(' "' + arg + '"')
     else:
         fd.write('\n')
         fd.write(indent + indentstep + '"' + lines[0] + '\n')
