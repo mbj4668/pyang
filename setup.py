d0865b5de964de418812f0ced6bdb8925a104375
lada.lhotka@gmail.com <lada.lhotka@gmail.com@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2012-03-09 11:50:04 +0000

    Fix issue 54: extend the distutils procedure.

--- setup.py
+++ setup.py
@@ -1,10 +1,48 @@
 from distutils.core import setup
+from distutils.dist import Distribution
 import pyang
 import glob
 import os
+import re
 
 modules = glob.glob(os.path.join('modules', '*.yang'))
 xslt = glob.glob(os.path.join('xslt', '*.xsl'))
+
+class PyangDist(Distribution):
+
+      """The purpose of this subclass of Distribution is to extend the
+      install procedure with preprocessing of shell scripts and man
+      pages so that they reflect the actual installation prefix, which
+      may be changed through the --prefix option.
+      """
+
+      def preprocess_files(self, prefix):
+            """Insert the installation prefix where necessary.
+
+            The template files have the '.in' suffix and the locations
+            are marked with '@prefix@'.
+            """
+            if prefix is None: return
+            files = ("bin/yang2dsdl", "man/man1/yang2dsdl.1")
+            regex = re.compile("^(.*)/usr/local(.*)$")
+            for f in files:
+                  inf = open(f)
+                  cnt = inf.readlines()
+                  inf.close()
+                  ouf = open(f,"w")
+                  for line in cnt:
+                        mo = regex.search(line)
+                        if mo is None:
+                              ouf.write(line)
+                        else:
+                              ouf.write(mo.group(1) + prefix + mo.group(2) + "\n")
+                  ouf.close()
+
+      def run_commands(self):
+            opts = self.command_options
+            if "install" in opts:
+                  self.preprocess_files(opts["install"].get("prefix", ("", None))[1])
+            Distribution.run_commands(self)
 
 setup(name='pyang',
       version=pyang.__version__,
@@ -12,6 +50,7 @@ setup(name='pyang',
       author_email='mbj@tail-f.com',
       description="A YANG (RFC 6020) validator and converter",
       url='http://code.google.com/p/pyang',
+      distclass=PyangDist,
       scripts=['bin/pyang', 'bin/yang2html', 'bin/yang2dsdl'],
       packages=['pyang', 'pyang.plugins', 'pyang.translators'],
       data_files=[('.', ['LICENSE']),
