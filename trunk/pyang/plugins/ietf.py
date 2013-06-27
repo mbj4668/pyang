237b0ff1fa388753914375a87aa75f458edc6905
mbj4668@gmail.com <mbj4668@gmail.com@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2013-06-27 15:28:43 +0000

    added reference to rfc section in ietf plugin error messages

--- trunk/pyang/plugins/ietf.py
+++ trunk/pyang/plugins/ietf.py
@@ -45,7 +45,7 @@ class IETFPlugin(plugin.PyangPlugin):
         statements.add_validation_var(
             '$chk_recommended',
             lambda keyword: keyword in _recommended_substatements)
-            
+
         statements.add_validation_fun(
             'grammar', ['$chk_default'],
             lambda ctx, s: v_chk_default(ctx, s))
@@ -62,7 +62,7 @@ class IETFPlugin(plugin.PyangPlugin):
 
         statements.add_validation_fun(
             'grammar', ['module', 'submodule'],
-            lambda ctx, s: v_chk_module_name(ctx, s))        
+            lambda ctx, s: v_chk_module_name(ctx, s))
 
         statements.add_validation_fun(
             'unique_name', ['module'],
@@ -71,32 +71,35 @@ class IETFPlugin(plugin.PyangPlugin):
         # register our error codes
         error.add_error_code(
             'IETF_EXPLICIT_DEFAULT', 4,
-            'IETF rule: statement "%s" is given with its default value "%s"')
+            'IETF rule (RFC 6087: 4.3): '
+            + 'statement "%s" is given with its default value "%s"')
         error.add_error_code(
             'IETF_MISSING_REQUIRED_SUBSTMT', 3,
-            'IETF rule: statement "%s" must have a "%s" substatement')
+            'IETF rule (%s): '
+            + 'statement "%s" must have a "%s" substatement')
         error.add_error_code(
             'IETF_MISSING_RECOMMENDED_SUBSTMT', 4,
-            'IETF rule: statement "%s" should have a "%s" substatement')
+            'IETF rule (%s): '
+            + 'statement "%s" should have a "%s" substatement')
         error.add_error_code(
             'IETF_BAD_NAMESPACE_VALUE', 4,
-            'IETF rule: namespace value should be "%s"')
+            'IETF rule (RFC 6087: 4.8): namespace value should be "%s"')
         error.add_error_code(
             'IETF_TOO_MANY_TOP_LEVEL_NODES', 4,
-            'IETF rule: too many top-level data nodes: %s')
+            'IETF rule (RFC 6087: 4.9): too many top-level data nodes: %s')
         error.add_error_code(
             'IETF_NO_MODULE_PREFIX', 4,
-            'IETF rule: no module name prefix used, suggest ietf-%s')
+            'IETF rule (RFC 6087: 4.1): '
+            + 'no module name prefix used, suggest ietf-%s')
 
         # override std error string
         error.add_error_code(
             'LONG_LINE', 4,
-            'IETF rule: line length %s exceeds %s characters')
+            'IETF rule (RFC formatting): line length %s exceeds %s characters')
         error.add_error_code(
             'LONG_IDENTIFIER', 3,
-            'IETF rule: identifier %s exceeds %s characters')
-        
-        
+            'IETF rule (RFC 6087: 4.2): identifier %s exceeds %s characters')
+
 _keyword_with_default = {
     'status': 'current',
     'mandatory': 'false',
@@ -107,33 +110,33 @@ _keyword_with_default = {
     }
 
 _required_substatements = {
-    'module': ('contact', 'organization', 'description', 'revision'),
-    'submodule': ('contact', 'organization', 'description', 'revision'),
-    'revision':('reference',),
-    'extension':('description',),
-    'feature':('description',),
-    'identity':('description',),
-    'typedef':('description',),
-    'grouping':('description',),
-    'grouping':('description',),
-    'augment':('description',),
-    'rpc':('description',),
-    'notification':('description',),
-    'container':('description',),
-    'leaf':('description',),
-    'leaf-list':('description',),
-    'list':('description',),
-    'choice':('description',),
-    'anyxml':('description',),
+    'module': (('contact', 'organization', 'description', 'revision'),
+               "RFC 6087: 4.7"),
+    'submodule': (('contact', 'organization', 'description', 'revision'),
+                  "RFC 6087: 4.7"),
+    'revision':(('reference',), "RFC 6087: 4.7"),
+    'extension':(('description',), "RFC 6087: 4.12"),
+    'feature':(('description',), "RFC 6087: 4.12"),
+    'identity':(('description',), "RFC 6087: 4.12"),
+    'typedef':(('description',), "RFC 6087: 4.11,4.12"),
+    'grouping':(('description',), "RFC 6087: 4.12"),
+    'augment':(('description',), "RFC 6087: 4.12"),
+    'rpc':(('description',), "RFC 6087: 4.12"),
+    'notification':(('description',), "RFC 6087: 4.12,4.14"),
+    'container':(('description',), "RFC 6087: 4.12"),
+    'leaf':(('description',), "RFC 6087: 4.12"),
+    'leaf-list':(('description',), "RFC 6087: 4.12"),
+    'list':(('description',), "RFC 6087: 4.12"),
+    'choice':(('description',), "RFC 6087: 4.12"),
+    'anyxml':(('description',), "RFC 6087: 4.12"),
     }
 
 _recommended_substatements = {
-    'must':('description',),
-    'when':('description',),
-    'enum':('description',),
-    'bit':('description',),
+    'must':(('description',), "RFC 6087: 4.12"),
+    'when':(('description',), "RFC 6087: 4.12"),
+    'enum':(('description',), "RFC 6087: 4.10,4.12"),
+    'bit':(('description',), "RFC 6087: 4.10,4.12"),
     }
-
 
 _ietf_namespace_prefix = 'urn:ietf:params:xml:ns:yang:'
 
@@ -145,27 +148,30 @@ def v_chk_default(ctx, stmt):
 
 def v_chk_required_substmt(ctx, stmt):
     if stmt.keyword in _required_substatements:
-        for r in _required_substatements[stmt.keyword]:
+        (required ,d) = _required_substatements[stmt.keyword]
+        for r in required:
             if stmt.search_one(r) is None:
                 err_add(ctx.errors, stmt.pos,
                         'IETF_MISSING_REQUIRED_SUBSTMT',
-                        (stmt.keyword, r))
+                        (s, stmt.keyword, r))
 
 def v_chk_recommended_substmt(ctx, stmt):
     if stmt.keyword in _recommended_substatements:
-        for r in _recommended_substatements[stmt.keyword]:
+        (recommended, s) = _recommended_substatements[stmt.keyword]
+        for r in recommended:
             if stmt.search_one(r) is None:
                 err_add(ctx.errors, stmt.pos,
                         'IETF_MISSING_RECOMMENDED_SUBSTMT',
-                        (stmt.keyword, r))
+                        (s, stmt.keyword, r))
 
 def v_chk_namespace(ctx, stmt):
     if not stmt.arg == _ietf_namespace_prefix + stmt.i_module.arg:
         err_add(ctx.errors, stmt.pos, 'IETF_BAD_NAMESPACE_VALUE',
                 _ietf_namespace_prefix + stmt.i_module.arg)
-        
+
 def v_chk_top_level_nodes(ctx, stmt):
-    top = [x for x in stmt.i_children if x.keyword not in ['rpc','notification']]
+    top = [x for x in stmt.i_children
+           if x.keyword not in ['rpc','notification']]
     if len(top) > 1:
         err_add(ctx.errors, stmt.pos, 'IETF_TOO_MANY_TOP_LEVEL_NODES',
                 ", ".join([x.arg for x in top]))
