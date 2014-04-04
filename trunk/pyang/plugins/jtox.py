bcb9a87cf96668fd5a094446613b08f242214429
lada.lhotka@gmail.com <lada.lhotka@gmail.com@60aeb129-8f4d-0410-b1ed-9fe3d3cbfe06>
2014-04-04 19:01:24 +0000

    JTOX: make the plugin use the new unique_prefixes function.

--- trunk/pyang/plugins/jtox.py
+++ trunk/pyang/plugins/jtox.py
@@ -25,12 +25,7 @@ import os
 import json
 
 from pyang import plugin, statements, error
-
-mods = {}
-"""Dictionary containing module prefixes and URIs.
-
-   Keys are module names, values are pairs [prefix, uri].
-"""
+from pyang.util import unique_prefixes
 
 def pyang_plugin_init():
     plugin.register_plugin(JtoXPlugin())
@@ -44,71 +39,56 @@ class JtoXPlugin(plugin.PyangPlugin):
         ctx.implicit_errors = False
 
     def emit(self, ctx, modules, fd):
-        emit_jtox(modules, ctx, fd)
+        """Main control function.
+        """
+        for (epos, etag, eargs) in ctx.errors:
+            if error.is_error(error.err_level(etag)):
+                raise error.EmitError("JTOX plugin needs a valid module")
+        tree = {}
+        mods = { m.i_modulename : [p, m.search_one("namespace").arg]
+                 for m,p in unique_prefixes(ctx).items() }
+        for module in modules:
+            self.process_children(module, tree)
+        json.dump({"modules": mods, "tree": tree}, fd)
 
-def emit_jtox(modules, ctx, fd):
-    """Main control function.
-    """
-    for (epos, etag, eargs) in ctx.errors:
-        if error.is_error(error.err_level(etag)):
-            raise error.EmitError("JTOX plugin needs a valid module")
-    tree = {}
-    prefixes = []
-    def unique_prefix(p):
-        """Disambiguate the module prefix."""
-        suffix = 0
-        while p == "nc" or p in prefixes:
-            p += "%d" % suffix
-            suffix += 1
-        return p
-    for module in modules:
-        uri = module.search_one("namespace").arg
-        prf = unique_prefix(module.i_prefix)
-        prefixes.append(prf)
-        mods[module.i_modulename] = [prf, uri]
-    for module in modules:
-        process_children(module, tree)
-    json.dump({"modules": mods, "tree": tree}, fd)
+    def process_children(self, node, parent):
+        """Process all children of `node`, except "rpc" and "notification".
+        """
+        for ch in node.i_children:
+            if ch.keyword in ["rpc", "notification"]: continue
+            if ch.keyword in ["choice", "case"]:
+                self.process_children(ch, parent)
+                continue
+            ndata = [ch.keyword]
+            if ch.keyword == "container":
+                ndata.append({})
+                self.process_children(ch, ndata[1])
+            elif ch.keyword == "list":
+                ndata.append({})
+                self.process_children(ch, ndata[1])
+                ndata.append([(k.i_module.i_modulename, k.arg)
+                              for k in ch.i_key])
+            elif ch.keyword in ["leaf", "leaf-list"]:
+                ndata.append(self.base_type(ch.search_one("type")))
+            modname = ch.i_module.i_modulename
+            if ch.arg in parent:
+                parent[ch.arg][modname] = ndata
+            else:
+                parent[ch.arg] = {modname: ndata}
 
-def process_children(node, parent):
-    """Process all children of `node`, except "rpc" and "notification".
-    """
-    for ch in node.i_children:
-        if ch.keyword in ["rpc", "notification"]: continue
-        if ch.keyword in ["choice", "case"]:
-            process_children(ch, parent)
-            continue
-        ndata = [ch.keyword]
-        if ch.keyword == "container":
-            ndata.append({})
-            process_children(ch, ndata[1])
-        elif ch.keyword == "list":
-            ndata.append({})
-            process_children(ch, ndata[1])
-            ndata.append([(k.i_module.i_modulename, k.arg)
-                          for k in ch.i_key])
-        elif ch.keyword in ["leaf", "leaf-list"]:
-            ndata.append(base_type(ch.search_one("type")))
-        modname = ch.i_module.i_modulename
-        if ch.arg in parent:
-            parent[ch.arg][modname] = ndata
+    def base_type(self, type):
+        """Return the base type of `type`."""
+        while 1:
+            if type.arg == "leafref":
+                node = type.i_type_spec.i_target_node
+            elif type.i_typedef is None:
+                break
+            else:
+                node = type.i_typedef
+            type = node.search_one("type")
+        if type.arg == "decimal64":
+            return [type.arg, int(type.search_one("fraction-digits").arg)]
+        elif type.arg == "union":
+            return [type.arg, [self.base_type(x) for x in type.i_type_spec.types]]
         else:
-            parent[ch.arg] = {modname: ndata}
-
-def base_type(type):
-    """Return the base type of `type`."""
-    while 1:
-        if type.arg == "leafref":
-            node = type.i_type_spec.i_target_node
-        elif type.i_typedef is None:
-            break
-        else:
-            node = type.i_typedef
-        type = node.search_one("type")
-    if type.arg == "decimal64":
-        return [type.arg, int(type.search_one("fraction-digits").arg)]
-    elif type.arg == "union":
-        return [type.arg, [base_type(x) for x in type.i_type_spec.types]]
-    else:
-        return type.arg
-
+            return type.arg
