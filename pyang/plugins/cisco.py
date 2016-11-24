c84541d74376e4a108a5151836a9e170e5e59e12
Mahesh Jethanandani <mjethanandani@gmail.com>
2016-11-23 16:32:52 -0800

    Added pyang plugins for Cisco, MEF and IEEE.

---
+++ pyang/plugins/cisco.py
@@ -0,0 +1,35 @@
+"""Cisco usage guidelines plugin
+"""
+
+import optparse
+import sys
+
+from pyang import plugin
+from pyang import statements
+from pyang import error
+from pyang.error import err_add
+from pyang.plugins import lint
+
+def pyang_plugin_init():
+    plugin.register_plugin(CiscoPlugin())
+
+class CiscoPlugin(lint.LintPlugin):
+    def __init__(self):
+        lint.LintPlugin.__init__(self)
+        self.namespace_prefixes = ['http://cisco.com/ns/yang/']
+        self.modulename_prefixes = ['Cisco']
+
+    def add_opts(self, optparser):
+        optlist = [
+            optparse.make_option("--cisco",
+                                 dest="cisco",
+                                 action="store_true",
+                                 help="Validate the module(s) according to " \
+                                 "Cisco rules."),
+            ]
+        optparser.add_options(optlist)
+
+    def setup_ctx(self, ctx):
+        if not ctx.opts.cisco:
+            return
+        self._setup_ctx(ctx)
