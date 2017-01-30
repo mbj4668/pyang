343a23f882f77c4e919c4765ff286da53576698a
Martin Björklund <mbj@tail-f.com>
2017-01-30 13:58:17 +0100

    Fixes #301 - fix bug w/ refine of leafref leaf

--- pyang/statements.py
+++ pyang/statements.py
@@ -1563,7 +1563,7 @@ def v_expand_1_uses(ctx, stmt):
     for ch in refined:
         # after refinement, we need to re-run some of the tests, e.g. if
         # the refinement added a default value it needs to be checked.
-        v_recheck_target(ctx, ch)
+        v_recheck_target(ctx, ch, reference=False)
 
 def v_inherit_properties(ctx, stmt, child=None):
     def iter(s, config_value, allow_explicit):
@@ -2181,18 +2181,21 @@ def v_reference_deviation_4(ctx, stmt):
         return
     if hasattr(stmt.i_target_node, 'i_this_not_supported'):
         return
-    v_recheck_target(ctx, stmt.i_target_node)
+    v_recheck_target(ctx, stmt.i_target_node, reference=True)
 
-def v_recheck_target(ctx, t):
+def v_recheck_target(ctx, t, reference=False):
     if t.keyword == 'leaf':
         v_type_leaf(ctx, t)
-        v_reference_leaf_leafref(ctx, t)
+        if reference:
+            v_reference_leaf_leafref(ctx, t)
     elif t.keyword == 'leaf-list':
         v_type_leaf_list(ctx, t)
-        v_reference_leaf_leafref(ctx, t)
+        if reference:
+            v_reference_leaf_leafref(ctx, t)
     elif t.keyword == 'list':
         t.i_is_validated = False
-        v_reference_list(ctx, t)
+        if reference:
+            v_reference_list(ctx, t)
 
 ### Unused definitions phase
 
@@ -2715,7 +2718,8 @@ def validate_leafref_path(ctx, stmt, path_spec, path,
             err_add(ctx.errors, pathpos, 'LEAFREF_MULTIPLE_KEYS',
                     (ptr.i_module.i_modulename, ptr.arg, stmt.arg, stmt.pos))
         if ((hasattr(stmt, 'i_config') and stmt.i_config == True) and
-            ptr.i_config == False and not accept_non_config_target):
+            hasattr(ptr, 'i_config') and ptr.i_config == False
+            and not accept_non_config_target):
             err_add(ctx.errors, pathpos, 'LEAFREF_BAD_CONFIG',
                     (stmt.arg, ptr.arg, ptr.pos))
         if ptr == stmt:
