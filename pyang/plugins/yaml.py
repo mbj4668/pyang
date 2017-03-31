8380d3b8d976f23b603db5d28d7806d785a78eec
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2017-03-31 07:22:38 +0530

    correct yaml format

--- pyang/plugins/yaml.py
+++ pyang/plugins/yaml.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This plugin translate YANG module into YAML format
+This plugin translate YANG module into YAML format http://yaml.org/
 """
 
 from pyang import plugin
@@ -19,42 +19,40 @@ class YAMLPlugin(plugin.PyangPlugin):
         ctx.implicit_errors = False
 
     def emit(self, ctx, modules, fd):
-        for (epos, etag, eargs) in ctx.errors:
-            if error.is_error(error.err_level(etag)):
-                raise error.EmitError("YAML plugin needs a valid module")
-
+        fd.write("---" + "\n")
+        parent = None
         for module in modules:
-            self.process_children(module,fd)
+            self.process_children(module, parent, fd)
 
-    def process_children(self, node, fd, indent=0):
+    def process_children(self, node, parent, fd, indent=0):
         """
         Process all children of `node`, except "rpc" and "notification".
         """
-        indentStr = ' ' * indent
-        fd.write(indentStr + "- %s:%s" % (node.arg, node.keyword)+"\n")
-        indent = indent + 2
+        is_indent = False
+        if parent == None:
+            fd.write(' ' * indent + "%s: " % (node.arg)+"\n")
+            indent = indent + 2
+
+        if parent is not None and parent.keyword == "case":
+            indent = indent + 2
+
         for ch in node.i_children:
             if ch.keyword in ["rpc", "notification"]: continue
-            if ch.keyword in ["container", "list", "choice"]:
-                self.process_children(ch, fd, indent+2)
+            if ch.keyword in ["container", "list", "choice", "case"]:
+                parent = ch
+                fd.write(' ' * indent + "%s: " % (ch.arg)+"\n")
+                self.process_children(ch, parent, fd, indent+2)
             elif ch.keyword in ["leaf", "leaf-list"]:
-                fd.write(' ' * indent + "%s: %s" \
-                        % (ch.arg, self.base_type(ch.search_one("type")))+"\n")
-
-    def base_type(self, type):
-        """Return the base type of `type`."""
-        while 1:
-            if type.arg == "leafref":
-                node = type.i_type_spec.i_target_node
-            elif type.i_typedef is None:
-                break
-            else:
-                node = type.i_typedef
-            type = node.search_one("type")
-        if type.arg == "decimal64":
-            return [type.arg, int(type.search_one("fraction-digits").arg)]
-        elif type.arg == "union":
-            return [type.arg, [self.base_type(x) for x in type.i_type_spec.types]]
-        else:
-            return type.arg
+                if hasattr(ch, 'i_is_key'):
+                    is_indent = True
+                    fd.write(' ' * indent + "- %s:" % ch.arg + "\n")
+                    if not is_indent:
+                        indent = indent + 2
+                elif ch.keyword == "leaf-list":
+                    fd.write(' ' * indent + "%s:" % ch.arg + "\n")
+                else:
+                    if is_indent:
+                        indent = indent + 2
+                        is_indent = False
+                    fd.write(' ' * indent + "%s:" % ch.arg + "\n")
 
