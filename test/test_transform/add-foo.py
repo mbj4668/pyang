f1e044805a748aede54812a75af54e373ce890a2
William Lupton <wlupton@broadband-forum.org>
2018-11-30 11:44:34 +0000

    Add a transform() method (c.f. output format emit() methods)
    Also update tests and add some tests that apply multiple transforms in different orders.

--- test/test_transform/add-foo.py
+++ test/test_transform/add-foo.py
@@ -1,36 +1,23 @@
+from pyang import error
 from pyang import plugin
 from pyang import statements
 
-plugin_name = 'add-foo'
 
 def pyang_plugin_init():
     plugin.register_plugin(AddFoo())
 
 
 class AddFoo(plugin.PyangPlugin):
-    def add_opts(self, optparser):
-        pass
-
     def add_transform(self, xforms):
-        xforms[plugin_name] = self
+        xforms['add-foo'] = self
 
-    # what are the pros and cons for using pre-validate versus post-validate?
-    # probably pre-validate is usually better?
-    def post_validate_ctx(self, ctx, modules):
-        if plugin_name not in ctx.opts.transforms:
-            return
-
+    def transform(self, ctx, modules):
         for module in modules:
             for stmt in module.substmts:
                 if stmt.keyword == 'container':
                     foo = stmt.search_one('foo')
                     if not foo:
                         foo = add_leaf(stmt, 'foo', 'string')
-
-            # this is NECESSARY if the module was changed and any downstream
-            # processing relies on it having been validated, e.g. uses
-            # i_children (as does the tree output format)
-            statements.validate_module(ctx, module)
 
 
 def add_leaf(parent, name, type_name):
