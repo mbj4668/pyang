f3601c4415df9ce6b369df87a5f7d07c4a1e71e2
Miroslav Los <miroslav.los@pantheon.tech>
2020-01-24 18:24:33 +0100

    Cleanup and fixup selftest; comment-out unused error codes

--- test/selftest.py
+++ test/selftest.py
@@ -6,50 +6,54 @@ import sys
 import glob
 import subprocess
 
-
-from pyang import util
 from pyang import error
 from pyang import grammar
 from pyang import syntax
 
-def oscmd(cmd):
-    p = subprocess.Popen(cmd, shell=True,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
-    res = p.stdout.read()
-    err = p.stderr.read()
-    retcode = p.returncode
-    if len(res) > 0 and res[-1] == '\n':
-        res = res[:-1]
-    if len(err) > 0 and err[-1] == '\n':
-        err = err[:-1]
-    return (retcode, res, err)
 
-found_error = False
+def oscmd(cmd):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    out = p.stdout.read().rstrip('\n')
+    err = p.stderr.read().rstrip('\n')
+    retcode = p.returncode
+    return retcode, out, err
+
 
 def chk_error_codes():
-    global found_error
+    found_error = False
     files = glob.glob("../pyang/*.py") + glob.glob("../pyang/*/*.py")
-    del files[files.index('../pyang/error.py')]
-    filesstr = ' '.join(files)
-    for x in error.error_codes:
-        (retcode, res, err) = oscmd('grep %s %s' % (x, filesstr))
-        if retcode != 0:
-            sys.stderr.write("Error code: %s not used" % x)
+    # FIXME: plugins are not loaded and their error codes are not defined
+    # Moreover, some plugins set up their codes in setup_ctx, which requires
+    # e.g. option parsing and repository setup done as in the pyang script.
+    # They also create new 'statements', which do not have yin_map entries.
+    for code in error.error_codes:
+        retcode, out, _ = oscmd(['grep', '-e', r'\b%s\b' % code, '--'] + files)
+        # A used error code has its definition and uses - inner newlines in out
+        if retcode or out.count('\n') == 0:
+            sys.stderr.write("Error code: %s not used\n" % code)
             found_error = True
+    return found_error
+
 
 def chk_stmts():
-    stmtmaps = [(grammar.stmt_map, "grammar.stmt_map"),
-                (syntax.yin_map, "syntax.yin_map")]
-    for (map, name) in stmtmaps:
-        for stmt in map:
-            targets = util.listsdelete((map,name), stmtmaps)
-            for (tmap, tname) in targets:
-                if stmt not in tmap:
-                    sys.stderr.write("Stmt %s in %s not found in %s" % \
-                                         (stmt, name, tname))
+    found_error = False
+    stmt_maps = [(set(grammar.stmt_map), "grammar.stmt_map"),
+                 (set(syntax.yin_map), "syntax.yin_map")]
+    for stmt_map, name in stmt_maps:
+        for test_map, test_name in stmt_maps:
+            if test_map is not stmt_map:
+                for stmt in stmt_map - test_map:
+                    sys.stderr.write("Stmt %s in %s not found in %s\n"
+                                     % (stmt, name, test_name))
+                    found_error = True
+    return found_error
 
-chk_error_codes()
-chk_stmts()
-if found_error:
-    sys.exit(1)
+
+if __name__ == '__main__':
+    found_error = any([
+        chk_error_codes(),
+        chk_stmts(),
+    ])
+    if found_error:
+        sys.exit(1)
