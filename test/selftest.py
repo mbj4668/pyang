9bb50e4caa2faaa2ca869a7badf2372fed6110f2
Miroslav Los <miroslav.los@pantheon.tech>
2020-01-27 11:38:57 +0100

    Selftest check more error codes from plugins; move main code to main.

--- test/selftest.py
+++ test/selftest.py
@@ -22,16 +22,39 @@ def oscmd(cmd):
 
 def chk_error_codes():
     found_error = False
-    files = glob.glob("../pyang/*.py") + glob.glob("../pyang/*/*.py")
-    # FIXME: plugins are not loaded and their error codes are not defined
+    # Plugins are not loaded and their error codes are not defined
     # Moreover, some plugins set up their codes in setup_ctx, which requires
     # e.g. option parsing and repository setup done as in the pyang script.
     # They also create new 'statements', which do not have yin_map entries.
-    for code in error.error_codes:
+    # Invoking the pyang error code listing function should list them all.
+    retcode, out, err = oscmd(
+        ['pyang', '--list-errors', '--lint', '--ieee', '--ietf'])
+    if retcode:
+        sys.stderr.write('Cannot list errors from pyang\n')
+        if err:
+            for line in err.split('\n'):
+                sys.stderr.write('[pyang] %s\n' % line)
+        found_error = True
+        listed_codes = set()
+    else:
+        error_categories = ('Error:', 'Minor Error:', 'Warning:')
+        listed_codes = set(line.strip().split()[-1] for line in out.split('\n')
+                           if line.startswith(error_categories))
+        for code in error.error_codes:
+            if code not in listed_codes:
+                sys.stderr.write('Error code: %s not listed by pyang\n' % code)
+                found_error = True
+
+    files = glob.glob("../pyang/*.py") + glob.glob("../pyang/*/*.py")
+    all_codes = sorted(set(error.error_codes) | listed_codes)
+    for code in all_codes:
         retcode, out, _ = oscmd(['grep', '-e', r'\b%s\b' % code, '--'] + files)
-        # A used error code has its definition and uses - inner newlines in out
+        # A used error code has has inner newlines in output for 2+ occurences
         if retcode or out.count('\n') == 0:
             sys.stderr.write("Error code: %s not used\n" % code)
+            if err:
+                for line in err.split('\n'):
+                    sys.stderr.write('[grep] %s\n' % line)
             found_error = True
     return found_error
 
@@ -50,10 +73,10 @@ def chk_stmts():
     return found_error
 
 
-if __name__ == '__main__':
-    found_error = any([
+def main():
+    return any([
         chk_error_codes(),
         chk_stmts(),
     ])
-    if found_error:
-        sys.exit(1)
+
+sys.exit(main())
