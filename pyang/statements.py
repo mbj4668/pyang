585b454b06429124b41c76f7308422de73a692b2
Jie Zhang <41674532+JieZhang0822@users.noreply.github.com>
2020-05-25 20:51:28 +0800

    Fix issue for #628 unexpected keyword can be used within the "augment… (#629)
    Fix issue #628: unexpected keyword can be used within the "augment" statement

--- pyang/statements.py
+++ pyang/statements.py
@@ -1785,6 +1785,18 @@ def v_expand_2_augment(ctx, stmt):
                 node.i_children.append(tmp)
                 tmp.parent = node
 
+    def is_expected_keyword(parent, child):
+        if parent.keyword == '__tmp_augment__':
+            return True
+        if parent.keyword == 'choice' and child.keyword != 'case':
+            (_arg_type, subspec) = grammar.stmt_map['case']
+        else:
+            (_arg_type, subspec) = grammar.stmt_map[parent.keyword]
+        subspec = grammar.flatten_spec(subspec)
+        if util.keysearch(child.keyword, 0, subspec) is None:
+            return False
+        return True
+
     for c in stmt.i_children:
         c.i_augment = stmt
         if hasattr(stmt, 'i_not_implemented'):
@@ -1814,10 +1826,16 @@ def v_expand_2_augment(ctx, stmt):
                         (stmt.arg, stmt.pos, c.arg, ch.pos))
                 return
         elif stmt.i_target_node.keyword == 'choice' and c.keyword != 'case':
-            # create an artificial case node for the shorthand
-            new_case = create_new_case(ctx, stmt.i_target_node, c, expand=False)
-            new_case.parent = stmt.i_target_node
-            v_inherit_properties(ctx, stmt.i_target_node, new_case)
+            if is_expected_keyword(stmt.i_target_node, c) is True:
+                # create an artificial case node for the shorthand
+                new_case = create_new_case(ctx, stmt.i_target_node, c, expand=False)
+                new_case.parent = stmt.i_target_node
+                v_inherit_properties(ctx, stmt.i_target_node, new_case)
+            else:
+                err_add(ctx.errors, stmt.pos, 'UNEXPECTED_KEYWORD_AUGMENT',
+                        (util.keyword_to_str(c.raw_keyword),
+                         util.keyword_to_str(stmt.i_target_node.raw_keyword),
+                         c.pos))
         elif (stmt.i_target_node.keyword not in
               ('container', 'list', 'choice', 'case', 'input',
                   'output', 'notification', '__tmp_augment__')):
@@ -1825,9 +1843,15 @@ def v_expand_2_augment(ctx, stmt):
             err_add(ctx.errors, stmt.pos, 'BAD_TARGET_NODE',
                     (nd.i_module.i_modulename, nd.arg, nd.keyword))
         else:
-            stmt.i_target_node.i_children.append(c)
-            c.parent = stmt.i_target_node
-            v_inherit_properties(ctx, stmt.i_target_node, c)
+            if is_expected_keyword(stmt.i_target_node, c) is True:
+                stmt.i_target_node.i_children.append(c)
+                c.parent = stmt.i_target_node
+                v_inherit_properties(ctx, stmt.i_target_node, c)
+            else:
+                err_add(ctx.errors, stmt.pos, 'UNEXPECTED_KEYWORD_AUGMENT',
+                        (util.keyword_to_str(c.raw_keyword),
+                         util.keyword_to_str(stmt.i_target_node.raw_keyword),
+                         c.pos))
     for s in stmt.substmts:
         if s.keyword in _copy_augment_keywords:
             stmt.i_target_node.substmts.append(s)
