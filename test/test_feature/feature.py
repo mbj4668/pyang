13809193709b6298a47de380ebe4ef599cf68ea3
Martin Björklund <mbj4668@gmail.com>
2022-03-30 09:49:48 +0200

    robustified feature test

--- test/test_feature/feature.py
+++ test/test_feature/feature.py
@@ -5,6 +5,15 @@ def pyang_plugin_init():
     plugin.register_plugin(FeaturePlugin())
 
 
+def f(fs):
+    if fs is None:
+        return 'None'
+    elif len(fs) == 0:
+        return '[]'
+    else:
+        fs.sort()
+        return ','.join(fs)
+
 class FeaturePlugin(plugin.PyangPlugin):
     def add_transform(self, xforms):
         xforms['feature'] = self
@@ -12,5 +21,6 @@ class FeaturePlugin(plugin.PyangPlugin):
     def transform(self, ctx, modules):
         for module in modules:
             print('# %s: features %s exclude_features %s' %
-                  (module, ctx.features.get(module.arg, None),
-                   ctx.exclude_features.get(module.arg, None)))
+                  (module,
+                   f(ctx.features.get(module.arg, None)),
+                   f(ctx.exclude_features.get(module.arg, None))))
