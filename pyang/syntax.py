mbj4668/pyang

commit dad5c6898ecd618f0b27a9cd8318df49fe8711e0

Fix issue #809: revision-date parsed wrong if >1 @ in path
If `bin/pyang` is given a path to a YANG file which contains more than
one '@' character (e.g. if there's an '@' in a parent directory), the
`re_filename` regexp will detect the revision-date as being everything
from the first instances of the '@' char in the path, to the yang/tin
suffix(es).

This fails to match the expected 'YYYY-MM-DD' date format that the
revision-date in the filename should take and `pyang` fails.

This commit fixes `re_filename` regexp so that it disregards all
characters in the filepath before the last directory separator (based
on the value of `os.sep`) in the string.

pyang/syntax.py
@@ -1,5 +1,6 @@
 """Description of YANG & YIN syntax."""
 
+import os
 import re
 import shlex
 import sys
@@ -129,10 +130,12 @@ re_descendant_schema_nodeid = re.compile("^" + descendant_schema_nodeid + "$")
 re_deviate = re.compile(r"^(add|delete|replace|not-supported)$")
 
 # Not part of YANG syntax per se but useful for pyang in several places
-re_filename = re.compile(r"^([^@]*?)" +          # putative module name
-                         r"(?:@([^.]*?))?" +     # putative revision
-                         r"(?:\.yang|\.yin)*" +  # foo@bar.yang.yin.yang.yin ?
-                         r"\.(yang|yin)$")       # actual final extension
+re_filename = re.compile(
+    r"^(?:.*" + os.sep + r")?" +    # ignore all before os.sep
+    r"([^@]*?)" +                   # putative module name
+    r"(?:@([^.]*?))?" +             # putative revision
+    r"(?:\.yang|\.yin)*" +          # foo@bar.yang.yin.yang.yin ?
+    r"\.(yang|yin)$")               # actual final extension
 
 arg_type_map = {
     "identifier": lambda s: re_identifier.search(s) is not None,

test/test_issues/test_i809/Makefile
@@ -1,0 +1,2 @@
+test:
+	pyang -Werror project@2022-04-07/project@2022-04-07.yang

test/test_issues/test_i809/project@2022-04-07/project@2022-04-07.yang
@@ -1,0 +1,13 @@
+module project {
+  yang-version 1;
+  namespace "urn:at-char-in-path:project:1";
+  prefix p;
+
+  description "'@' char in parent dir of file confuses the revision date parsing";
+
+  revision 2022-04-07;
+
+  leaf foo {
+    type string;
+  }
+}

commit 23c51f163b1c20fcc9b697e9990c3ead92cd7924

Fix invalid regular expression on Windows
The code was trying to be portable by using the OS-specific path
separator in a regular expression. However, on Windows the path
separator is a backslash which is a special character in a regular
expression.

However, simply wrapping `os.pathsep` in `re.compile()` would not be the
right thing to do here. It is very common to also use paths with
Unix-style forward slashes on Windows, especially with portable projects
which want to use the same scripts on Unix. Since forward slashes are
not allowed in file names on Windows (and they can be used "instead of"
backslashes in a lot of contexts), using both is OK on Windows. Anyone
using backslashes in, say, Linux, will see a change of behavior, but
come on, that would not exactly be the sanest thing to do. Also, YANG
disallows a lot of funny characters in module names, so let's be
reasonable here.

Of course the best fix here would be to use something like pathlib for
path handling, and only apply the regex on actual file names, but I
would prefer to use pyang in Windows CI for our project without doing a
major refactoring here.

Fixes: dad5c68 Fix issue #809: revision-date parsed wrong if >1 @ in path

pyang/syntax.py
@@ -131,7 +131,7 @@ re_deviate = re.compile(r"^(add|delete|replace|not-supported)$")
 
 # Not part of YANG syntax per se but useful for pyang in several places
 re_filename = re.compile(
-    r"^(?:.*" + os.sep + r")?" +    # ignore all before os.sep
+    r"^(?:.*[/\\])?" +              # ignore all before path separator (either / or \)
     r"([^@]*?)" +                   # putative module name
     r"(?:@([^.]*?))?" +             # putative revision
     r"(?:\.yang|\.yin)*" +          # foo@bar.yang.yin.yang.yin ?