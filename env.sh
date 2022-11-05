c884728265d91feebacb3542a050d8fbc339aa66
ubaumann <github@m.ubaumann.ch>
2022-11-05 17:07:23 +0100

    Update tests to use pyang/scripts/*.py

--- env.sh
+++ env.sh
@@ -3,7 +3,6 @@
 # source this file to get environment setup for the
 # pyang below here
 
-export PATH="$PWD/bin:$PATH"
 export MANPATH="$PWD/man:$MANPATH"
 export PYTHONPATH="$PWD:$PYTHONPATH"
 export YANG_MODPATH="$PWD/modules:$YANG_MODPATH"
