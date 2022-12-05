aa237c1b0e57057fc5f1d538b3a67fb0f01621ed
ubaumann <github@m.ubaumann.ch>
2022-12-05 14:30:45 +0100

    Update README

--- env.sh
+++ env.sh
@@ -9,3 +9,10 @@ export YANG_MODPATH="$PWD/modules:$YANG_MODPATH"
 export PYANG_XSLT_DIR="$PWD/xslt"
 export PYANG_RNG_LIBDIR="$PWD/schema"
 export W="$PWD"
+
+if ! pyang --help &> /dev/null
+then
+    alias pyang="$PWD/pyang/scripts/pyang_tool.py"
+    alias json2xml="$PWD/pyang/scripts/json2xml.py"
+    alias yang2html="$PWD/pyang/scripts/yang2html.py"
+fi
