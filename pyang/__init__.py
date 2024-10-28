0c0e2d74eee0f0f1b315e86f0bc9d3a1f3012d7b
ygorelik <yangorelik@yahoo.com>
2024-10-28 15:13:27 -0700

    Bumped petch version to 2.6.1.1

--- pyang/__init__.py
+++ pyang/__init__.py
@@ -1,9 +1,4 @@
 """The pyang library for parsing, validating, and converting YANG modules"""
 
-<<<<<<< Updated upstream
 __version__ = '2.6.1.1'
 __date__ = '2024-10-28'
-=======
-__version__ = '2.5.0.m1'
-__date__ = '2021-07-08'
->>>>>>> Stashed changes
