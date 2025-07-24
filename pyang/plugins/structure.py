d3b75d4da2103bc8662edf09a235cc975a76d078
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2025-07-24 14:29:29 +0200

    Plugin structure: Validation of augment-structure
    Follow more closely the RFC 8791: YANG Data Structure Extensions.
    Fix the issue related to 'augment-structure' validation.
    Improve ietf-yang-structure-ext grammar to reflect the RFC.
    
    TODO:
      Check that 'must' statements, 'when' statements, ...
      located inside a structure do not reference outside
      the XPath context (the structure itself).

--- pyang/plugins/structure.py
+++ pyang/plugins/structure.py
@@ -42,13 +42,74 @@ def pyang_plugin_init():
         grammar.add_to_stmts_rules(add_to_stmts,
                                    [((module_name, stmt), occurance)])
 
-body_stmts = [
+    # Add validation functions
+    statements.add_validation_fun('expand_2',
+                                  [asx],
+                                  v_expand_2_augment_sx)
+    statements.add_validation_fun('expand_3',
+                                  [asx],
+                                  v_expand_3_augment_sx)
+
+    # Register special erro codes
+    error.add_error_code('BAD_AUGMENT_STRUCTURE_TARGET_NODE', 1,
+                         "target node of 'augment-structure' statement " +
+                         "must be 'structure' node")
+
+def v_expand_2_augment_sx(ctx, stmt):
+    """
+    First pass of two-pass augment expansion algorithm changed for
+    augment-structure.
+
+    First observation: since we validate each imported module, all
+    nodes that are augmented by other modules already exist.  For each
+    node in the path to the target node, if it does not exist, it
+    might get created by an augment later in this module.  This only
+    applies to nodes defined in our namespace (since all other modules
+    already are validated).  For each such node, we add a temporary
+    Statement instance, and store a pointer to it.  If we find such a
+    temporary node in the nodes we add, we replace it with our real
+    node, and delete it from the list of temporary nodes created.
+    When we're done with all augment statements, the list of temporary
+    nodes should be empty, otherwise it is an error.
+    """
+    statements.v_expand_2_augment(ctx, stmt)
+
+def v_expand_3_augment_sx(ctx, stmt):
+    """
+    Second pass of two-pass augment expansion algorithm changed for
+    augment-structure statement.
+
+    Find the (possibly expanded) target nodes again. The reason for
+    this is that stmt.i_target_node may point to a __tmp__augment__ node.
+    """
+    statements.v_expand_3_augment(ctx, stmt)
+    if not hasattr(stmt, 'i_target_node') or stmt.i_target_node is None:
+        err_add(ctx.errors, stmt.pos, 'BAD_NODE_IN_AUGMENT', ())
+    target = stmt.i_target_node
+    # Try to find toplevel 'structure' node parent
+    while (target.parent != None and
+           target.parent.keyword not in ('module', 'submodule')):
+        target = target.parent
+    if target.keyword != (module_name, 'structure'):
+        err_add(ctx.errors, stmt.pos, 'BAD_AUGMENT_STRUCTURE_TARGET_NODE', ())
+
+sx_body_stmts = [
+    ('must', '*'),
+    ('status', '?'),
     ('description', '?'),
     ('reference', '?'),
-    ('$interleave', grammar.data_def_stmts)
+    ('$interleave', [('typedef', '*'),
+                     ('grouping', '*')] +
+                    grammar.data_def_stmts),
     ]
 
-## FIXME: validate augment-structure
+asx_body_stmts = [
+    ('status', '?'),
+    ('description', '?'),
+    ('reference', '?'),
+    ('$interleave', [('case', '*')] +
+                    grammar.data_def_stmts),
+    ]
 
 structure_stmts = [
 
@@ -57,11 +118,11 @@ structure_stmts = [
     #  <list of keywords where <keyword> can occur>)
 
     ('structure', '*',
-     ('identifier', body_stmts),
+     ('identifier', sx_body_stmts),
      ['module', 'submodule']),
 
     ('augment-structure', '*',
-     ('absolute-schema-nodeid', body_stmts),
+     ('absolute-schema-nodeid', asx_body_stmts),
      ['module', 'submodule']),
 
 ]
